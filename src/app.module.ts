b9a00bd9254e0ee54bd87045f0442d92b40ab056
cherartem <artem138927@gmail.com>
2023-12-25 19:42:38 +0100

    chore: add new service for the database that extends prisma client

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { validateENVs } from './env.validation'
+import { DbModule } from './db/db.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       validate: validateENVs,
     }),
+    DbModule,
   ],
   controllers: [],
   providers: [],
