75609c134a8e89221633903bdd53d5b5eeef0a46
cherartem <artem138927@gmail.com>
2023-12-25 19:34:22 +0100

    build: set up env variables validation and config for accessing them

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { validateENVs } from './env.validation'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: validateENVs,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
