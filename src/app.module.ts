3035ce703a93333ba205ed87add9db23c0fb793f
cherartem <artem138927@gmail.com>
2023-12-26 23:04:05 +0100

    build: add env file path to the config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,14 @@ import { validateENVs } from './env.validation'
 import { DbModule } from './db/db.module'
 import { SystemModule } from './system/system.module'
 
+const NODE_ENV = process.env.NODE_ENV
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       validate: validateENVs,
       isGlobal: true,
+      envFilePath: !NODE_ENV ? '.env' : `.env.${NODE_ENV}`,
     }),
     DbModule,
     SystemModule,
