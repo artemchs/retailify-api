bfed07965b57f8f6b64d427746028bac9be69735
Artem Chernysh <artem_chernysh@outlook.com>
2024-05-25 10:51:49 +0200

    test(custom-operations): :white_check_mark: add integration tests to the resource

--- src/system/financial-transactions/custom-operations/tests/custom-operations.service.test.int.spec.ts
+++ src/system/financial-transactions/custom-operations/tests/custom-operations.service.test.int.spec.ts
@@ -2,6 +2,9 @@ import { DbService } from 'src/db/db.service'
 import { CustomOperationsService } from '../custom-operations.service'
 import { Test } from '@nestjs/testing'
 import { AppModule } from 'src/app.module'
+import { CreateCustomOperationDto } from '../dto/create-custom-operation.dto'
+import { BadRequestException, NotFoundException } from '@nestjs/common'
+import { UpdateCustomOperationDto } from '../dto/update-custom-operation.dto'
 
 describe('CustomOperationsService', () => {
   let service: CustomOperationsService
@@ -25,30 +28,147 @@ describe('CustomOperationsService', () => {
   })
 
   describe('create', () => {
-    it('should successfully create a new custom operation', async () => {})
+    const data: CreateCustomOperationDto = {
+      name: 'Test Operation',
+    }
+
+    it('should successfully create a new custom operation', async () => {
+      await service.create(data)
+
+      const operations = await db.customFinancialOperation.count()
+
+      expect(operations).toBe(1)
+    })
   })
 
   describe('findAll', () => {
-    it('should find all custom operations', async () => {})
+    beforeEach(async () => {
+      await db.customFinancialOperation.createMany({
+        data: [
+          {
+            name: 'Test Operation 1',
+          },
+          {
+            name: 'Test Operation 2',
+          },
+        ],
+      })
+    })
+
+    it('should find all custom operations', async () => {
+      const { items } = await service.findAll({})
+
+      expect(items.length).toBe(2)
+    })
   })
 
   describe('findOne', () => {
-    it('should find the requested custom operation', async () => {})
+    const id = 'Test Operation 1'
 
-    it('should throw an exception if the custom operation does not exist', async () => {})
+    beforeEach(async () => {
+      await db.customFinancialOperation.create({
+        data: {
+          id,
+          name: id,
+        },
+      })
+    })
+
+    it('should find the requested custom operation', async () => {
+      const data = await service.findOne(id)
+
+      expect(data).toBeDefined()
+      expect(data).not.toBeNull()
+      expect(data.id).toBe(id)
+    })
+
+    it('should throw an exception if the custom operation does not exist', async () => {
+      await expect(service.findOne('non-existent')).rejects.toThrow(
+        NotFoundException,
+      )
+    })
   })
 
   describe('update', () => {
-    it('should update the requested custom operation', async () => {})
+    const id = 'Test Operation 1'
 
-    it('should throw an exception if the custom operation does not exist', async () => {})
+    beforeEach(async () => {
+      await db.customFinancialOperation.create({
+        data: {
+          id,
+          name: id,
+        },
+      })
+    })
+
+    const data: UpdateCustomOperationDto = {
+      name: 'Updated Test Operation 1',
+    }
+
+    it('should update the requested custom operation', async () => {
+      await service.update(id, data)
+
+      const customFinancialOperation =
+        await db.customFinancialOperation.findUnique({
+          where: {
+            id,
+          },
+        })
+
+      expect(customFinancialOperation?.name).toBe(data.name)
+    })
+
+    it('should throw an exception if the custom operation does not exist', async () => {
+      await expect(service.update('non-existent', data)).rejects.toThrow(
+        NotFoundException,
+      )
+    })
   })
 
   describe('remove', () => {
-    it('should remove the requested custom operation', async () => {})
+    const id = 'Test Operation 1'
 
-    it('should throw an exception if the custom operation does not exist', async () => {})
+    beforeEach(async () => {
+      await db.customFinancialOperation.create({
+        data: {
+          id,
+          name: id,
+        },
+      })
+    })
 
-    it('should throw an exception if the custom operation is connected to transactions', async () => {})
+    it('should remove the requested custom operation', async () => {
+      await service.remove(id)
+
+      const operations = await db.customFinancialOperation.count()
+
+      expect(operations).toBe(0)
+    })
+
+    it('should throw an exception if the custom operation does not exist', async () => {
+      await expect(service.remove('non-existent')).rejects.toThrow(
+        NotFoundException,
+      )
+    })
+
+    it('should throw an exception if the custom operation is connected to transactions', async () => {
+      const id = 'Test Operation 2'
+
+      await db.customFinancialOperation.create({
+        data: {
+          id,
+          name: id,
+          transactions: {
+            create: {
+              amount: 1,
+              direction: 'CREDIT',
+              type: 'OTHER',
+            },
+          },
+        },
+      })
+
+      await expect(service.remove(id)).rejects.toThrow(BadRequestException)
+    })
   })
 })
