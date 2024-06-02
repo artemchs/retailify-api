f4e4cb8f487c4d573946e0a7a024b843809b7439
Artem Chernysh <artem_chernysh@outlook.com>
2024-06-02 21:37:11 +0200

    refactor(app): :wrench: set global prefix to 'api'

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import helmet from 'helmet'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const port = process.env.PORT ? parseInt(process.env.PORT) : 3000
+  app.setGlobalPrefix('api')
   app.use(helmet())
   app.useGlobalPipes(
     new ValidationPipe({
