e986f22cc58218987ba14e52ab811415a80fe4a7
Artem Chernysh <artem_chernysh@outlook.com>
2024-06-06 11:31:06 +0200

    chore(storefront): :building_construction: generate customers resource

--- src/storefront/storefront.module.ts
+++ src/storefront/storefront.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common'
 import { AuthModule } from './auth/auth.module'
+import { CustomersModule } from './customers/customers.module'
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, CustomersModule],
 })
 export class StorefrontModule {}
