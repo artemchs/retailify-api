aea2f800b2efc584ef5b70650ce127e7e04900de
Artem Chernysh <artem_chernysh@outlook.com>
2024-06-13 17:44:41 +0200

    fix(storefront): :bug: import the sms module in the customers module

--- src/storefront/customers/customers.module.ts
+++ src/storefront/customers/customers.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
 import { CustomersService } from './customers.service'
 import { CustomersController } from './customers.controller'
+import { SmsModule } from '../../sms/sms.module'
 
 @Module({
   controllers: [CustomersController],
   providers: [CustomersService],
+  imports: [SmsModule],
 })
 export class CustomersModule {}
